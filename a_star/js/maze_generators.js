26c3a17b4993d74ede367f68da9790aab5392138
fanot <59705773+fanot@users.noreply.github.com>
2022-04-15 14:17:03 +0700

    master

---
+++ a_star/js/maze_generators.js
@@ -0,0 +1,245 @@
+"use strict";
+
+function get_neighbours(cell, distance)
+{
+	let up = [cell[0], cell[1] - distance];
+	let right = [cell[0] + distance, cell[1]];
+	let down = [cell[0], cell[1] + distance];
+	let left = [cell[0] - distance, cell[1]];
+	return [up, right, down, left];
+}
+
+function random_int(min, max)
+{
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function fill()
+{
+	for (let i = 0; i < grid.length; i++)
+		for (let j = 0; j < grid[0].length; j++)
+			add_wall(i, j);
+}
+
+function fill_walls()
+{
+	for (let i = 0; i < grid.length; i++)
+		for (let j = 0; j < grid[0].length; j++)
+			if (i % 2 == 0 || j % 2 == 0)
+				add_wall(i, j);
+}
+
+function enclose()
+{
+	for (let i = 0; i < grid.length; i++)
+	{
+		add_wall(i, 0);
+		add_wall(i, grid[0].length - 1);
+	}
+
+	for (let j = 0; j < grid[0].length; j++)
+	{
+		add_wall(0, j);
+		add_wall(grid.length - 1, j);
+	}
+}
+
+
+
+function kruskal_algorithm()
+{
+	fill_walls();
+	let nb_areas = 0;
+	let wall_list = [];
+
+	for (let i = 1; i < grid.length - 1; i++)
+		for (let j = 1; j < grid[0].length - 1; j++)
+		{
+			if (i % 2 == 1 && j % 2 == 1)
+			{
+				nb_areas++;
+				grid[i][j] = nb_areas;
+				place_to_cell(i, j).classList.add("visited_cell");
+			}
+
+			if ((i + j) % 2 == 1)
+				wall_list.push([i, j]);
+		}
+
+	my_interval = window.setInterval(function()
+	{
+		while (true)
+		{
+			if (nb_areas == 1)
+			{
+				clearInterval(my_interval);
+				clear_grid();
+				generating = false;
+				return;
+			}
+
+			let index = random_int(0, wall_list.length);
+			let wall = wall_list[index];
+			wall_list.splice(index, 1);
+			let cell_pair;
+
+			if (grid[wall[0] - 1][wall[1]] > -1)
+				cell_pair = [grid[wall[0] - 1][wall[1]], grid[wall[0] + 1][wall[1]]];
+			else
+				cell_pair = [grid[wall[0]][wall[1] - 1], grid[wall[0]][wall[1] + 1]];
+
+			if (cell_pair[0] != cell_pair[1])
+			{
+				for (let i = 1; i < grid.length - 1; i += 2)
+					for (let j = 1; j < grid[0].length - 1; j += 2)
+						if (grid[i][j] == cell_pair[0])
+							grid[i][j] = cell_pair[1];
+
+				remove_wall(wall[0], wall[1]);
+				place_to_cell(wall[0], wall[1]).classList.add("visited_cell");
+				nb_areas--;
+				return;
+			}
+		}
+	}, 29);
+}
+
+function prim_algorithm()
+{
+	fill();
+	let first_cell = [1, 1];
+	remove_wall(first_cell[0], first_cell[1]);
+	place_to_cell(first_cell[0], first_cell[1]).classList.add("visited_cell");
+	grid[first_cell[0]][first_cell[1]] = 1;
+	let wall_list = [];
+	let list = get_neighbours(first_cell, 1);
+
+	for (let i = 0; i < list.length; i++)
+		if (list[i][0] > 0 && list[i][0] < grid.length - 1 && list[i][1] > 0 && list[i][1] < grid[0].length - 1)
+			wall_list.push(list[i]);
+
+	my_interval = window.setInterval(function()
+	{
+		while (true)
+		{
+			if (wall_list.length == 0)
+			{
+				clearInterval(my_interval);
+				clear_grid();
+				generating = false;
+				return;
+			}
+
+			let index = random_int(0, wall_list.length);
+			let wall = wall_list[index];
+			wall_list.splice(index, 1);
+			let cell_pair;
+
+			if (wall[0] % 2 == 0)
+				cell_pair = [[wall[0] - 1, wall[1]], [wall[0] + 1, wall[1]]];
+			else
+				cell_pair = [[wall[0], wall[1] - 1], [wall[0], wall[1] + 1]];
+
+			let new_cell;
+			let valid = false;
+
+			if (grid[cell_pair[0][0]][cell_pair[0][1]] < 1)
+			{
+				new_cell = cell_pair[0];
+				valid = true;
+			}
+
+			else if (grid[cell_pair[1][0]][cell_pair[1][1]] < 1)
+			{
+				new_cell = cell_pair[1];
+				valid = true;
+			}
+
+			if (valid)
+			{
+				remove_wall(wall[0], wall[1]);
+				remove_wall(new_cell[0], new_cell[1]);
+				place_to_cell(wall[0], wall[1]).classList.add("visited_cell");
+				place_to_cell(new_cell[0], new_cell[1]).classList.add("visited_cell");
+				grid[new_cell[0]][new_cell[1]] = 1;
+				let list = get_neighbours(new_cell, 1);
+
+				for (let i = 0; i < list.length; i++)
+					if (list[i][0] > 0 && list[i][0] < grid.length - 1 && list[i][1] > 0 && list[i][1] < grid[0].length - 1)
+						wall_list.push(list[i]);
+
+				return;
+			}
+		}
+	}, 28);
+}
+
+
+function maze_generators()
+{
+	let start_temp = start_pos;
+	let target_temp = target_pos;
+	hidden_clear();
+	generating = true;
+
+	if (start_temp[0] % 2 == 0)
+	{
+		if (start_temp[0] == grid.length - 1)
+			start_temp[0] -= 1;
+		else
+			start_temp[0] += 1;
+	}
+
+	if (start_temp[1] % 2 == 0)
+	{
+		if (start_temp[1] == 0)
+			start_temp[1] += 1;
+		else
+			start_temp[1] -= 1;
+	}
+
+	if (target_temp[0] % 2 == 0)
+	{
+		if (target_temp[0] == grid.length - 1)
+			target_temp[0] -= 1;
+		else
+			target_temp[0] += 1;
+	}
+
+	if (target_temp[1] % 2 == 0)
+	{
+		if (target_temp[1] == 0)
+			target_temp[1] += 1;
+		else
+			target_temp[1] -= 1;
+	}
+
+	place_to_cell(start_pos[0], start_pos[1]).classList.remove("start");
+	place_to_cell(start_temp[0], start_temp[1]).classList.add("start");
+	place_to_cell(target_pos[0], target_pos[1]).classList.remove("target");
+	place_to_cell(target_temp[0], target_temp[1]).classList.add("target");
+	start_pos = start_temp;
+	target_pos = target_temp;
+
+	grid_clean = false;
+
+	if (document.querySelector("#slct_2").value == "1")
+		randomized_depth_first();
+
+	else if (document.querySelector("#slct_2").value == "2")
+		kruskal_algorithm();
+
+	else if (document.querySelector("#slct_2").value == "3")
+		prim_algorithm();
+
+	else if (document.querySelector("#slct_2").value == "4")
+		wilson_algorithm();
+
+	else if (document.querySelector("#slct_2").value == "5")
+		aldous_broder_algorithm();
+
+	else if (document.querySelector("#slct_2").value == "6")
+		recursive_division();
+}
