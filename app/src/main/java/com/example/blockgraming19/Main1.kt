c96b012970ebafb5a1068a4c026d181b82dc4111
Necusic <97069468+Necusic@users.noreply.github.com>
2022-05-10 13:07:49 +0700

    Починил проект

--- app/src/main/java/com/example/blockgraming19/Main1.kt
+++ app/src/main/java/com/example/blockgraming19/Main1.kt
@@ -8,6 +8,9 @@ object Main {
     fun main(args: Array<String>) {
         val input = String(File("C:\\Users\\User\\AndroidStudioProjects\\BlockGraming19\\app\\src\\main\\java\\com\\example\\blockgraming19\\program.txt").readBytes())
         val tokens = Lexer(input).tokenize()
+        for (token in tokens) {
+            println(token)
+        }
         val program = Parser(tokens).parse()
         println(program.toString())
         program.execute()
--- app/src/main/java/com/example/blockgraming19/MainActivity.kt
+++ app/src/main/java/com/example/blockgraming19/MainActivity.kt
@@ -2,16 +2,47 @@ package com.example.blockgraming19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.contentValuesOf
+
 
 class MainActivity : AppCompatActivity() {
+    var rusult:TextView? = null
+    var input:EditText? = null
+    var input1:EditText? = null
+    var resButton:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        }
+        rusult = findViewById(R.id.textView)
+
+        input1 = findViewById(R.id.input1)
+        resButton = findViewById(R.id.Result)
+
+//        resButton?.setOnClickListener {
+//            val num1:String = input1?.text.toString()
+//            val tokens = Lexer(num1).tokenize()
+//            val statements = Parser(tokens).parse()
+//            val a = mutableListOf<String>()
+//            for (i in statements){
+//                i.execute()
+//                a.add(i.toString())
+//
+//            }
+//            rusult?.setText(a.toString())
+//
+//        }
+
     }
 
+
+
+}
 
 
 
