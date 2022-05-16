bc2c06df271ee0c2330191c92aadf725f01a7e80
Necusic <97069468+Necusic@users.noreply.github.com>
2022-05-16 19:22:51 +0700

    Много изменений

---
+++ app/src/main/java/com/example/blockgraming19/TokenType.java
@@ -0,0 +1,48 @@
+package com.example.blockgraming19;
+public enum TokenType {
+
+    NUMBER,
+    HEX_NUMBER,
+    WORD,
+    TEXT,
+    
+    // keyword
+    PRINT,
+    IF,
+    ELSE,
+    WHILE,
+    FOR,
+    DO,
+    BREAK,
+    CONTINUE,
+    DEF,
+    RETURN,
+    
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    EQ,
+    EQEQ,
+    EXCL,
+    EXCLEQ,
+    LT,
+    LTEQ,
+    GT,
+    GTEQ,
+    
+    BAR,
+    BARBAR,
+    AMP,
+    AMPAMP,
+    
+    LPAREN, // (
+    RPAREN, // )
+    LBRACKET, // [
+    RBRACKET, // ]
+    LBRACE, // {
+    RBRACE, // }
+    COMMA, // ,
+    
+    EOF
+}
