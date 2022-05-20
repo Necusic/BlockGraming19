bf7ed1cb722ae2d5c26da4fe9573f8b2c7f4ced7
Necusic <97069468+Necusic@users.noreply.github.com>
2022-05-20 22:30:50 +0700

    почти готово

--- app/src/main/java/com/example/blockgraming19/MainActivity.kt
+++ app/src/main/java/com/example/blockgraming19/MainActivity.kt
@@ -54,4 +54,39 @@ class MainActivity : AppCompatActivity() {
 
 
 
+<<<<<<< Updated upstream
+=======
+            val view = layoutInflater.inflate(R.layout.variable, null)
+            val deleteField = view.findViewById<View>(R.id.button2) as Button
+            deleteField.setOnClickListener {
+                try {
+                    (view.parent as LinearLayout).removeView(view)
+                    (allEds as ArrayList<View>).remove(view)
+                } catch (ex: IndexOutOfBoundsException) {
+                    ex.printStackTrace()
+                }
+            }
+            val text = view.findViewById<View>(R.id.input) as EditText
+            text.setText("")
+            (allEds as ArrayList<View>).add(view)
+            linear.addView(view)
+        }
+        val showDataBtn = findViewById<View>(R.id.button3) as Button
+        showDataBtn.setOnClickListener {
+
+            val items = arrayOfNulls<String>((allEds as ArrayList<View>).size)
+            for (i in (allEds as ArrayList<View>).indices) {
+                items[i] = ((allEds as ArrayList<View>).get(i).findViewById<View>(R.id.input) as EditText).text.toString() + ((allEds as ArrayList<View>).get(i).findViewById<View>(R.id.input1) as EditText).text.toString()
+                Log.e(items.toString(), items.toString(), )
+                val print = findViewById<View>(R.id.tex) as TextView
+                print.setText("")
+
+
+                    val num = global + ((allEds as ArrayList<View>).get(i).findViewById<View>(R.id.input) as EditText).text.toString() + ((allEds as ArrayList<View>).get(i).findViewById<View>(R.id.input1) as EditText).text.toString()
+                    val tokens = Lexer(num).tokenize()
+                    val statements = Parser(tokens, print as TextView).parse()
+                    statements.execute()
+
+            }
+>>>>>>> Stashed changes
 
